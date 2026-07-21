ad89cc866e4271cc012682dbe6981fcec4af35f1
leaddevops <leaddevops@gmail.com>
2026-07-21 13:56:11 +0530

    custom

--- 03-custom-networking/enable-custom-networking.sh
+++ 03-custom-networking/enable-custom-networking.sh
@@ -39,6 +39,8 @@ done
 
 kubectl set env daemonset aws-node -n kube-system AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG=true
 kubectl set env daemonset aws-node -n kube-system ENI_CONFIG_LABEL_DEF=topology.kubernetes.io/zone
+kubectl set env daemonset aws-node -n kube-system ENABLE_PREFIX_DELEGATION=true
+kubectl get node -o custom-columns=NODE:.metadata.name,PODS:.status.capacity.pods
 kubectl rollout restart daemonset aws-node -n kube-system
 kubectl rollout status daemonset aws-node -n kube-system
 
