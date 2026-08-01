c942c6df33bcca540f7a824092e35129e6c1996e
leaddevops <leaddevops@gmail.com>
2026-08-01 21:54:35 +0530

    eni fix

--- terraform/01-modules/eks/main.tf
+++ terraform/01-modules/eks/main.tf
@@ -159,7 +159,7 @@ metadata:
 spec:
   subnet: ${each.value}
   securityGroups:
-  - ${var.cluster_security_group_id}
+  - ${aws_eks_cluster.this.vpc_config[0].cluster_security_group_id}
 EKSOF
 EOF
   }
